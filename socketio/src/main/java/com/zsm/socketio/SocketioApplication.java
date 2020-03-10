b9987b2cee2faf17996fbe347a8497ffe502aad4
Shengming <453752303@qq.com>
2020-03-10 18:27:01 +0800

    Add socketio demo.

---
+++ socketio/src/main/java/com/zsm/socketio/SocketioApplication.java
@@ -0,0 +1,16 @@
+package com.zsm.socketio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SocketioApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SocketioApplication.class, args);
+    }
+
+}
